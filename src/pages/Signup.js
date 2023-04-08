3b3bd58166503943bace144f9437921ee2bed51b
wagolemusa <homiemusa@gmail.com>
2023-04-09 00:37:48 +0300

    Workrd on Login and Sigin up

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,7 +3,8 @@ import { Alert } from "react-bootstrap";
 import './signup.css'
 import { Link } from "react-router-dom";
 import { useSignupMutation } from "../services/appApi";
-
+import log from './log.png'
+import pto from './pto.avif'
 
 
 const Signup = () => {
@@ -22,35 +23,46 @@ const Signup = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-6">
+                    <div className="login-box1">
+                    <div className="row">
+                        <div className="col">
+                            <h1>Signup </h1>
+                        </div>
+                        <div className="col">
+                            <img src={log} alt='log'/>
+                        </div>
+                    </div><br/>
+                   
                     <form style={{ width: "100%" }} onSubmit={handleSignup}>
                     {isError && <Alert variant="danger">{error.data}</Alert>}
-                    <div class="form-outline mb-3">
+                    <div class="mb-3">
                     <input type="text" id="form12" class="form-control" placeholder="Full Names" required
                         value={name}  onChange={(e) => setName(e.target.value)}
                     />
                     </div>
-                    <div class="form-outline mb-3">
+                    <div class="mb-3">
                     <input type="text" id="form12" class="form-control" placeholder="Email" required
                         value={email}  onChange={(e) => setEmail(e.target.value)}
                     />
                     </div>
-                    <div class="form-outline mb-3">
-                    <input type="text" id="form12" class="form-control" placeholder="Password" required
+                    <div class="mb-3">
+                    <input type="password" id="form12" class="form-control" placeholder="Password" required
                         value={password}  onChange={(e) => setPassword(e.target.value)}
                     />
                     </div>
-                    <button type="submit" class="btn btn-primary" disabled={isLoading}>Signup</button>
+                    <button type="submit" class="btn-login" disabled={isLoading}>Signup</button>
                     <p className="pt-3 text-center">
                         Don't have an account? <Link to="/login">Login</Link>{" "}
                     </p>
                     </form>
                     </div>
-                    
+                    </div>
                     <div className="col-md-6">
-                        <div className="signup__image_conatiner">
+                        <div className="signupImage">
+                            <img src={pto} alt='log'/>
 
                         </div>
-                        
+  
                     </div>
                 </div>
             </div>
